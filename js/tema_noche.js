21b6eabfe15601e0fa60053e79176bfba2735838
nfelipepperalta <121313125+nfelipepperalta@users.noreply.github.com>
2023-01-01 21:04:14 -0300

    Actualización

---
+++ js/tema_noche.js
@@ -0,0 +1,4 @@
+document.getElementById('btn').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#5c5470';
+    //document.main.style.backgroundColor = '#dbd8e3'
+});
