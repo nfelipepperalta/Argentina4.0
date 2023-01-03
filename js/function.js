0dc2e45e821423eaa2e6ffd54176973ed22eba3b
Felipe <nfelipepperalta@outlook.com>
2023-01-02 21:44:53 -0300

    nueva actulizacion js

--- js/function.js
+++ js/function.js
@@ -1,4 +1,21 @@
-document.getElementById('bcolor').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#FF0000';   
-});
+window.onload = function(){
+    
+    document.getElementById('bcolor').onclick = function() {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        const main = document.getElementById('hojacv')
+        main.style.backgroundColor = 'gray';
+        document.getElementById('bcolor').setAttribute('disabled', true);
+        document.getElementById('ncolor').removeAttribute('disabled');
+    };
+    document.getElementById('ncolor').onclick = function() {
+        document.body.style.backgroundColor = '#F8F8FF';
+        document.body.style.color = 'black';
+        const main = document.getElementById('hojacv')
+        main.style.backgroundColor = 'white';
+        document.getElementById('bcolor').removeAttribute('disabled')
+        document.getElementById('ncolor').setAttribute('disabled', true)
+    };
+};
+
 
